630f7fc22dfd2c3a3870b6c8c0ebb8760d8f0dc9
JinJieBeWater <cjj20030813@qq.com>
2026-03-31 00:29:46 +0800

    feat(hardware): add szpi esp32-s3 board config

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,24 +1,79 @@
 #include <Arduino.h>
+#include <esp_ota_ops.h>
 
-// put function declarations here:
-// int myFunction(int, int);
+constexpr int kBootKeyPin = 0;
+constexpr unsigned long kSerialWaitTimeoutMs = 2000;
+constexpr unsigned long kButtonPollDelayMs = 50;
+
+bool gLastButtonPressed = false;
+
+void waitForSerial() {
+  unsigned long startedAt = millis();
+
+  while (!Serial && millis() - startedAt < kSerialWaitTimeoutMs) {
+    delay(10);
+  }
+}
+
+void printRuntimeCheck() {
+  const esp_partition_t* partition = esp_ota_get_running_partition();
+
+  Serial.println();
+  Serial.println("=== SZPI ESP32-S3 runtime check ===");
+  Serial.printf("Chip model: %s\n", ESP.getChipModel());
+  Serial.printf("Chip revision: %d\n", ESP.getChipRevision());
+  Serial.printf("SDK version: %s\n", ESP.getSdkVersion());
+  Serial.printf("CPU frequency: %lu MHz\n", ESP.getCpuFreqMHz());
+  Serial.printf("Flash size: %u MB\n", ESP.getFlashChipSize() / (1024 * 1024));
+
+#if ARDUINO_USB_CDC_ON_BOOT
+  Serial.println("USB CDC on boot: enabled");
+#else
+  Serial.println("USB CDC on boot: disabled");
+#endif
+
+#if defined(BOARD_HAS_PSRAM)
+  Serial.println("BOARD_HAS_PSRAM: defined");
+#else
+  Serial.println("BOARD_HAS_PSRAM: not defined");
+#endif
+
+  Serial.printf("PSRAM detected: %s\n", psramFound() ? "yes" : "no");
+  Serial.printf("PSRAM size: %u MB\n", ESP.getPsramSize() / (1024 * 1024));
+  Serial.printf("Free heap: %u bytes\n", ESP.getFreeHeap());
+  Serial.printf("Free PSRAM: %u bytes\n", ESP.getFreePsram());
+
+  if (partition != nullptr) {
+    Serial.printf("Running partition: %s (%u bytes)\n", partition->label, partition->size);
+  }
+
+  Serial.println("===============================");
+}
+
+void printButtonState(bool pressed) {
+  Serial.println(pressed ? "BOOT pressed" : "BOOT released");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  // int result = myFunction(2, 3);
+  pinMode(kBootKeyPin, INPUT_PULLUP);
 
-  pinMode(14,OUTPUT);
+  Serial.begin(115200);
+  waitForSerial();
+  Serial.println("SZPI ESP32-S3 first flash test");
+  Serial.println("BOOT key is on GPIO0.");
+  printRuntimeCheck();
+
+  gLastButtonPressed = digitalRead(kBootKeyPin) == LOW;
+  printButtonState(gLastButtonPressed);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(14,HIGH);
-  delay(500);
-  digitalWrite(14,LOW);
-  delay(500);
-}
+  bool buttonPressed = digitalRead(kBootKeyPin) == LOW;
 
-// put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
+  if (buttonPressed != gLastButtonPressed) {
+    gLastButtonPressed = buttonPressed;
+    printButtonState(buttonPressed);
+  }
+
+  delay(kButtonPollDelayMs);
+}
